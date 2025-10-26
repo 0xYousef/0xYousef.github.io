255162c6435ca5fa9a021e67579a6102a7409d21
Yousef Mohaned <youssief192678@fci.bu.edu.eg>
2025-10-26 03:39:56 +0300

    Build Portfolio Architecture

---
+++ src/app/shared/explore-blogs/explore-blogs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-blogs',
+  imports: [],
+  templateUrl: './explore-blogs.html',
+  styleUrl: './explore-blogs.css',
+})
+export class ExploreBlogs {
+
+}
---
+++ src/app/shared/section-layout/section-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-layout',
+  imports: [],
+  templateUrl: './section-layout.html',
+  styleUrl: './section-layout.css',
+})
+export class SectionLayout {
+
+}
