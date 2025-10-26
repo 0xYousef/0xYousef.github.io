4c85fc8776d4f9fb9bd44c9c3e11a584bc960e0c
Yousef Mohaned <youssief192678@fci.bu.edu.eg>
2025-10-26 05:35:01 +0300

    create global css and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Aboutme } from './features/aboutme/aboutme';
+import { Services } from './features/services/services';
+import { Blogs } from './features/blogs/blogs';
+import { CategoryBlogs } from './features/blogs/category-blogs/category-blogs';
+import { Blog } from './features/blogs/blog/blog';
+import { Contactme } from './features/contactme/contactme';
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'about-me', component: Aboutme},
+    {path:'services', component: Services},
+    {
+    path: 'blogs',
+    children: [
+        {path: '', component: Blogs },
 
-export const routes: Routes = [];
+        {path: ':type', component: CategoryBlogs },
+
+        {path: ':type/:id', component: Blog },
+    ],
+    },
+    {path: 'contact-me', component: Contactme},
+    {path: '==', redirectTo: ''},
+];
---
+++ src/app/features/blogs/category-blogs/category-blogs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-blogs',
+  imports: [],
+  templateUrl: './category-blogs.html',
+  styleUrl: './category-blogs.css',
+})
+export class CategoryBlogs {
+
+}
